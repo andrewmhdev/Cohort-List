3d3d97456fca39b6b75317c9e394e175b0952a28
Andrew H <andrewdev@Andrews-MacBook-Air.local>
2022-05-10 17:02:47 -0400

    Add student model

---
+++ CohortList/CohortList/Model/Student.swift
@@ -0,0 +1,18 @@
+//
+//  Student.swift
+//  CohortList
+//
+//  Created by Andrew H on 5/10/22.
+//
+
+import Foundation
+
+class Student {
+    let name: String
+    let cohortID: Int
+    
+    init(name: String, studentID: Int) {
+        self.name = name
+        self.cohortID = studentID
+    }
+}
