b2c0bab5d992b0b415baa67d0f5e6a2fec5a1367
Andrew H <andrewdev@Andrews-MacBook-Air.local>
2022-05-11 19:52:52 -0400

    Added clear, delete and save button functionality for the table view

--- CohortList/CohortList/ViewControllers/StudentDetailViewController.swift
+++ CohortList/CohortList/ViewControllers/StudentDetailViewController.swift
@@ -15,19 +15,50 @@ class StudentDetailViewController: UIViewController {
     @IBOutlet weak var cohortNumberTextField: UITextField!
     @IBOutlet weak var studentDescriptionTextView: UITextView!
     
+    
+    //MARK: Reciever var
+    var studentReciever: Student?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateViews(studentToDisplay: studentReciever)
+        
 
     }
     
+    
     @IBAction func saveButtonTapped(_ sender: Any) {
+        guard let name = studentNameTextField.text,
+              let cohortID = cohortNumberTextField.text,
+              let studentDescription = studentDescriptionTextView.text else {return}
+        if let student = studentReciever {
+            StudentController.sharedInstance.updateStudent(newName: name, newCohort: Int(cohortID) ?? 0, newStudentDetails: studentDescription, studentToUpdate: student)
+        } else {
+            StudentController.sharedInstance.createStudent(name: name, cohortID: Int(cohortID) ?? 0, studentDescription: studentDescription)
+        }
+        self.navigationController?.popViewController(animated: true)
     }
     @IBAction func clearButtonTapped(_ sender: Any) {
+        clearData()
     }
     @IBAction func deleteButtonTapped(_ sender: Any) {
+        guard let student = studentReciever else {return}
+            StudentController.sharedInstance.deleteStudent(studentToDelete: student)
+        self.navigationController?.popViewController(animated: true)
     }
-    // MARK: - Navigation
 
-
-
+    func updateViews(studentToDisplay: Student?) {
+        guard let studentToDisplay = studentToDisplay else {return}
+        studentNameTextField.text = studentToDisplay.name
+        cohortNumberTextField.text = "\(studentToDisplay.cohortID)"
+        studentDescriptionTextView.text = studentToDisplay.studentDescription
+        
+    }
+    
+    func clearData() {
+        studentNameTextField.text = ""
+        cohortNumberTextField.text = ""
+    }
+    
+    
 }
--- CohortList/CohortList/ViewControllers/StudentListTableViewController.swift
+++ CohortList/CohortList/ViewControllers/StudentListTableViewController.swift
@@ -9,14 +9,11 @@ import UIKit
 
 class StudentListTableViewController: UITableViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
     }
-    @IBAction func addButtonTapped(_ sender: Any) {
 
-}
-    
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -34,49 +31,23 @@ class StudentListTableViewController: UITableViewController {
         return cell
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetailVC" {
+            if let index = tableView.indexPathForSelectedRow {
+                if let destination = segue.destination as? StudentDetailViewController {
+                    let student = StudentController.sharedInstance.students[index.row]
+                    destination.studentReciever = student
+                        
+                }
+            }
+        }
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
     }
-    */
 
 }
