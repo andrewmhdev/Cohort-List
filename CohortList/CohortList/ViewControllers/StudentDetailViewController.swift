d7a6c7bf549b1244c8819b6b71aac0f594394566
Andrew H <andrewdev@Andrews-MacBook-Air.local>
2022-05-11 17:13:26 -0400

    Added outlets and buttons for detailed view

--- CohortList/CohortList/ViewControllers/StudentDetailViewController.swift
+++ CohortList/CohortList/ViewControllers/StudentDetailViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class StudentDetailViewController: UIViewController {
 
+    @IBOutlet weak var studentNameTextField: UITextField!
+    
+    @IBOutlet weak var cohortLabel: UILabel!
+    @IBOutlet weak var cohortNumberTextField: UITextField!
+    @IBOutlet weak var studentDescriptionTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
+    @IBAction func saveButtonTapped(_ sender: Any) {
+    }
+    @IBAction func clearButtonTapped(_ sender: Any) {
+    }
+    @IBAction func deleteButtonTapped(_ sender: Any) {
+    }
     // MARK: - Navigation
 
 
